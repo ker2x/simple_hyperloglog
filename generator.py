1c0ff03f62967ec4d63687f49ab75d31697183ce
Laurent Laborde <kerdezixe@gmail.com>
2014-08-07 11:44:05 +0200

    removing useless redis stuff

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
-import redis
+#!/usr/bin/env python
+
 import random
 import string
 
@@ -8,14 +9,7 @@ python generate.py > TEST.txt
 ./hyperloglog TEST.txt
 '''
 
-#r = redis.Redis(host='localhost', port=6379, db=0)
 
 for i in range(1000000):
 	chars = "".join( [random.choice(string.letters).lower() for i in xrange(3)] )
 	print chars
-	#r.execute_command('PFADD', 'mkey', chars)
-
-
-
-
-
